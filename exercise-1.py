ac8f95d800f89bc13884896d31f1a5251fa3db78
NSopheakneath <2025295ngoun@aupp.edu.kh>
2025-07-04 19:39:45 +0700

    neath (exercise 1 and 2)

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,9 @@
+
+
 def replace_last(numbers):
+    if len(numbers)==0:
+        return numbers
+    new_list= [numbers[-1]] + numbers[:-1]
+    return new_list
+
     ...
--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,6 @@
-def index_power(numnbers, n):
+def index_power(numbers, n):
+    if n > len(numbers):
+        return -1        
+    else:
+        return numbers[n] ** n
     ...
